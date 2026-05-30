1cbb45110467083e82e6f772b9501a2a4f60f7b7
송다인 <dain.song@myrealtrip.com>
2026-05-30 14:23:59 +0900

    Initial Commit

---
+++ GithubSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubSearch
+//
+//  Created by 송다인 on 5/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubSearch/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by 송다인 on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
